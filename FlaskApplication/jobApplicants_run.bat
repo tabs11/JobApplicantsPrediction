c433ba28fce695560e7703fe51a26034590bd58b
Nuno <nunomartins_11@hotmail.com>
2025-02-22 22:26:20 +0000

    Add files via upload

--- FlaskApplication/jobApplicants_run.bat
+++ FlaskApplication/jobApplicants_run.bat
@@ -1,4 +1,4 @@
 @echo off
 call "%USERPROFILE%\anaconda3\Scripts\activate.bat"
-python "%USERPROFILE%\Desktop\Mestrado_IA\AlgoritmosML\FAI_Projeto02_30783_33461-20250126T213332Z-001\FAI_Projeto02_30783_33461\JobApplicants\application.py"
+python application.py
 pause
